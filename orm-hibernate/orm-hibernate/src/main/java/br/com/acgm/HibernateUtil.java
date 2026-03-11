fe09738c91e7be9d5f6acf6ec84aaa098c88c57a
Antonio Carlos Gomes Marinho <acgm@windows.cin.ufpe.br>
2026-03-11 12:37:28 -0300

    criando classe hibernate util

---
+++ orm-hibernate/orm-hibernate/src/main/java/br/com/acgm/HibernateUtil.java
@@ -0,0 +1,36 @@
+package br.com.acgm;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+	
+	public static EntityManagerFactory factory = null;
+	
+	static {
+		
+		init();
+	}
+	
+	private static void init() {
+		
+		try {
+			
+				if(factory == null) 
+					factory = Persistence.createEntityManagerFactory("orm-hibernate");
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static EntityManager getEntityManager() {
+		
+		return factory.createEntityManager();
+	}
+
+}
---
+++ orm-hibernate/orm-hibernate/src/main/java/br/com/acgm/Test.java
@@ -0,0 +1,12 @@
+package br.com.acgm;
+
+public class Test {
+	
+	@org.junit.Test
+	public void test() {
+		
+		HibernateUtil.getEntityManager();
+		
+	}
+
+}
