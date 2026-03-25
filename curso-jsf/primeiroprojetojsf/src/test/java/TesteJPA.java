69cc96384306a969a6342e129dfaba1c969444d1
Antonio Carlos Gomes Marinho <ant.marinho@outlook.com>
2026-03-25 12:58:28 -0300

    Testando o JPA e a criação de tabelas

---
+++ curso-jsf/primeiroprojetojsf/src/test/java/TesteJPA.java
@@ -0,0 +1,13 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("primeiroprojetojsf");
+
+    }
+
+}
