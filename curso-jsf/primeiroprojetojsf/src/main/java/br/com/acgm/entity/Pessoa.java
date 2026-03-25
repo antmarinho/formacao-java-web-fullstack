704720ce3f4f783ad6836355831324fc21b81b31
Antonio Carlos Gomes Marinho <ant.marinho@outlook.com>
2026-03-25 12:29:28 -0300

    comecando mapeamento jpa

---
+++ curso-jsf/primeiroprojetojsf/src/main/java/br/com/acgm/entity/Pessoa.java
@@ -0,0 +1,76 @@
+package br.com.acgm.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Pessoa  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private Integer idade;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtnsc;
+
+    public Pessoa() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Date getDtnsc() {
+        return dtnsc;
+    }
+
+    public void setDtnsc(Date dtnsc) {
+        this.dtnsc = dtnsc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
